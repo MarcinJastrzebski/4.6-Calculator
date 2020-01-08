c94c3510fa5140cc60db25f1249171c0d87a3caa
Marcin Jastrzębski <jaczomb@gmail.com>
2020-01-08 20:34:47 +0100

    add multiply function

--- src/main/java/com/kodilla/kodillacourse/Calculator.java
+++ src/main/java/com/kodilla/kodillacourse/Calculator.java
@@ -8,6 +8,9 @@ public class Calculator {
     public int subtractTwoDigits(int a, int b){
         return a-b;
     }
+    public int multiplyTwoDigits(int a, int b){
+        return a*b;
+    }
 
     public static void main(String[] args) {
         Calculator calc = new Calculator();
